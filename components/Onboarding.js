3f8bec72b15336fe3090253a830c01289413f114
uchikuch <ucheazinge@gmail.com>
2021-02-19 20:40:29 +0100

    Animated FlatList Carousel

---
+++ components/Onboarding.js
@@ -0,0 +1,51 @@
+import React, { useState, useRef } from "react";
+import { StyleSheet, Text, View, FlatList, Animated } from "react-native";
+
+import OnboardingItem from "../components/OnboardingItem";
+import slides from "../slides";
+
+const Onboarding = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const slidesRef = useRef(null);
+
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator
+          pagingEnabled
+          bounces={false}
+          keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Onboarding;
---
+++ slides.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: "1",
+    title: "Quick & Easy Payment",
+    description:
+      "Grow your business by accepting card payments with the new card reader.",
+    image: require("./assets/images/creditcard.png"),
+  },
+  {
+    id: "2",
+    title: "Smart Point of Sale",
+    description:
+      "Complete point of sale software tailored to your business needs.",
+    image: require("./assets/images/authentication.png"),
+  },
+  {
+    id: "3",
+    title: "Instant Notification",
+    description:
+      "Instant notifications let your quickly see new purchases and messages.",
+    image: require("./assets/images/notifications.png"),
+  },
+  {
+    id: "4",
+    title: "Customise Everything",
+    description: "Adjust your system to speed up your checkout.",
+    image: require("./assets/images/customize.png"),
+  },
+];
